2684ecdf9950cd54be3255f5ecf4d3c1838ca1f9
Ezequiel Ramos <ezequiel.ramos@institutozonaoeste.edu.ar>
2025-06-17 18:06:08 -0300

    --

--- SQLquerys.sql
+++ SQLquerys.sql
@@ -2,20 +2,20 @@ CREATE TABLE Ventas(
 idVta INT PRIMARY KEY IDENTITY(1,1),
 fechaVta DATE,
 totalVta INT,
-TipoPago NVARCHAR(13),
+TipoPago NVARCHAR(13)
 );
 
 CREATE TABLE Productos(
 idProd INT PRIMARY KEY IDENTITY(1,1),
 descriProd NVARCHAR(100),
-PrecioUnitario INT,
+PrecioUnitario INT
 );
 
 CREATE TABLE DetallesVenta(
 ID INT PRIMARY KEY IDENTITY(1,1),
 IDVenta INT FOREIGN KEY REFERENCES Ventas(idVta),
 IDProd INT FOREIGN KEY REFERENCES Productos(idProd),
-subtotal INT,
+subtotal INT
 );
 
 select * from Productos
@@ -23,4 +23,35 @@ select * from DetallesVenta
 select * from Ventas
 
 INSERT INTO Productos
-VALUES('Cerveza Brahama', 2800)
+VALUES('Cerveza Quilmes', 2800)
+
+
+CREATE PROCEDURE sp_InsertarDetalleVenta
+    @IDVenta INT,
+    @IDProd INT,
+    @Cantidad INT
+AS
+BEGIN
+    DECLARE @PrecioUnitario INT;
+    DECLARE @Subtotal INT;
+
+    -- Obtener el precio unitario del producto
+    SELECT @PrecioUnitario = PrecioUnitario FROM Productos WHERE idProd = @IDProd;
+
+    -- Calcular subtotal
+    SET @Subtotal = @Cantidad * @PrecioUnitario;
+
+    -- Insertar el detalle con subtotal calculado
+    INSERT INTO DetallesVenta (IDVenta, IDProd, subtotal)
+    VALUES (@IDVenta, @IDProd, @Subtotal);
+END
+
+CREATE PROCEDURE sp_RegistrarVta
+    @fechaVta DATE,
+    @totalVta INT,
+    @TipoPago NVARCHAR(13)
+AS
+BEGIN
+    INSERT INTO Ventas (fechaVta, totalVta, TipoPago)
+    VALUES (@fechaVta, @totalVta, @TipoPago);
+END
